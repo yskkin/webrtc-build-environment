e74e1fc5885ac696430d6db8b0720f4126d8cdd9
Yoshiyuki Kinjo <yskkin@gmail.com>
2014-03-21 20:27:53 +0900

    Add gclient installation to scm recipe

--- site-cookbooks/scm/recipes/default.rb
+++ site-cookbooks/scm/recipes/default.rb
@@ -12,3 +12,17 @@
     action :install
   end
 end
+
+git "/opt/depot_tool" do
+  repository "https://chromium.googlesource.com/chromium/tools/depot_tools.git"
+  action :checkout
+end
+
+bash "export path" do
+  rc_file = "~vagrant/.bashrc"
+  export = "export PATH=$PATH:/opt/depot_tool"
+
+  user "vagrant"
+  code "echo '#{export}' >> #{rc_file}"
+  not_if "grep '#{export}' #{rc_file}"
+end
